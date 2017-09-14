7e401c3ac504e42e39037d64544a668006963b53
own3mall <own3mall@gmail.com>
2017-09-14 00:49:43 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Portuguese(Brazil)/modules/news.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('manage_listings', "Gestão das suas novidades");
+define('add_new_listing', "Publicar Notícias");
+define('your_current_listings', "Suas notícias atuais");
+define('edit', "Editar");
+define('date', "Data");
+define('images', "Imagens");
+define('title', "Título");
+define('description', "Conteúdo da notícia");
+define('written_by', "Escrito por");
+define('details', "Consulte mais informação");
+define('modify', "Modificar");
+define('save', "Salvar");
+define('delete', "Apagar");
+define('sure_to_delete', "Tem certeza de que deseja excluir essas notícias?");
+define('go_back', "Voltar a trás!");
+define('new_added_success', "A notícia foi postada com sucesso!");
+define('add_another', "Adicionar outro");
+define('or_message', "ou");
+define('please_select', "Por favor selecione");
+define('submit', "Submeter");
+define('edit_listing', "Editar as notícias");
+define('modifications_saved', "Os novos valores foram salvos com sucesso!");
+define('modify_images', "Modifique as novas imagens do artigo");
+define('upload_more_images', "Carregar mais imagens");
+define('latest_news', "Últimas notícias");
+define('search_results', "Procurar Resultados");
+define('no_results', "Nenhuma notícia foi encontrada.");
+define('config_options', "Opções de notícias");
+define('date_format', "Formato de data");
+define('results_per_page', "Notícias por Página");
+define('enable_search', "Ativar mecanismo de pesquisa");
+define('image_quality', "Qualidade da imagem (0-100)");
+define('max_image_width', "Largura máxima da imagem (px)");
+define('gallery_theme', "Prateleira da galeria de imagens");
+define('images_bottom', "Posição da galeria de imagens");
+define('img_bottom', "Sob o artigo");
+define('img_right', "Lado direito do artigo");
+define('no_word', "Não");
+define('yes_word', "Sim");
+define('no_access', "Você não tem o direito de acessar esta página. Sua ação será registrada para inspeção adicional.");
+define('write_permission_required', "É necessária permissão de gravação");
+define('fix_permission', "Corrija as permissões. O módulo não funcionará como pretendido até você corrigir todas as permissões.");
+define('check_permissions', "Verificar permissões");
+define('OK_permission', "As permissões necessárias estão todas OK!");
+define('empty_title', "Preencha o título");
+define('empty_description', "Por favor, preencha o conteúdo");
+define('empty_author', "Preencha o nome do autor");
+define('GD_fail', "A extensão GD NÃO é carregada no seu servidor. Carregamento de imagens desativado.");
+define('search_news', "Pesquisar as Notícias");
+define('help', "ajuda");
+define('help_date', "Obter ajuda sobre o formato diferente da data");
+define('ID_invalid', "O ID da notícia não existe");
+define('ID_not_set', "O ID da notícia não está configurado");
+define('unauthorized_access', "Acesso não autorizado de");
+define('WYSIWYG', "WYSIWYG editor");
+define('tinymce_lang', "Linguagem Tiny MCE ");
+define('da', "Dinamarquês");
+define('de', "Alemão");
+define('en_GB', "Inglês");
+define('es', "Espanhol");
+define('fi', "Finlandês");
+define('fr_FR', "Francês");
+define('it', "Italiano");
+define('pl', "Polaco");
+define('pt_PT', "Português");
+define('ru', "Russo");
+define('tinymce_skin', "Tiny MCE skin");
+define('tinymce_skin_custom', "Você precisa carregar sua própria capa personalizada em <b>modules/news/js/tinymce/skins/custom/</b> pasta para poder usar esta skin. Se você selecioná-lo sem fazê-lo, você encontrará problemas. Crie sua própria capa personalizada aqui <a href='http://skin.tinymce.com/' target='_blank'>http://skin.tinymce.com/</a>.");
+define('safe_HTML', "Purificador HTML");
+define('safe_HTML_en', "Purificador HTML habilitado");
+define('safe_HTML_dis', "Purificador HTML desativado");
+define('safe_HTML_en_info', "O conteúdo HTML do artigo na visualização detalhada será purificado. Isto levará na remoção de algumas tags HTML como iframes. Edite o arquivo <b>modules/news/config.php</b> Para alterar a configuração 'safe_HTML' de valor '1'(ativado) para o valor '0'(desativado) para desativar esse comportamento e permitir o uso de HTML completo sem restrições.");
+define('safe_HTML_dis_info', "O conteúdo HTML do artigo na visualização detalhada não será purificado. Edite o arquivo <b>modules/news/config.php</b> Para alterar a configuração 'safe_HTML' do valor '0' (desativado) para o valor '1' (ativado) para habilitar apenas o uso seguro de tags HTML.");
+?>
