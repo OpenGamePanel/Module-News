f2100af01956ed71ba272d0dfffeb62fb0e9cd74
own3mall <own3mall@gmail.com>
2020-04-26 13:22:17 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Finnish(Finland)/modules/news.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_manage_listings', "Manage your news");
+define('OGP_LANG_add_new_listing', "Post News");
+define('OGP_LANG_your_current_listings', "Your current news");
+define('OGP_LANG_edit', "Edit");
+define('OGP_LANG_date', "Date");
+define('OGP_LANG_images', "Images");
+define('OGP_LANG_title', "Title");
+define('OGP_LANG_description', "News Content");
+define('OGP_LANG_written_by', "Written by");
+define('OGP_LANG_details', "Read More");
+define('OGP_LANG_modify', "Modify");
+define('OGP_LANG_save', "Save");
+define('OGP_LANG_delete', "Delete");
+define('OGP_LANG_sure_to_delete', "Are you sure that you would like to delete these news?");
+define('OGP_LANG_go_back', "Go Back");
+define('OGP_LANG_new_added_success', "The news has been posted successfully!");
+define('OGP_LANG_add_another', "Add another one");
+define('OGP_LANG_or_message', "or");
+define('OGP_LANG_please_select', "Please Select");
+define('OGP_LANG_submit', "Submit");
+define('OGP_LANG_edit_listing', "Edit the news");
+define('OGP_LANG_modifications_saved', "The new values have been saved successfully!");
+define('OGP_LANG_modify_images', "Modify the new article images");
+define('OGP_LANG_upload_more_images', "Upload more images");
+define('OGP_LANG_latest_news', "Latest News");
+define('OGP_LANG_search_results', "Search Results");
+define('OGP_LANG_no_results', "No news was found.");
+define('OGP_LANG_config_options', "News Options");
+define('OGP_LANG_date_format', "Date Format");
+define('OGP_LANG_results_per_page', "News per Page");
+define('OGP_LANG_enable_search', "Enable search engine");
+define('OGP_LANG_image_quality', "Image quality (0-100)");
+define('OGP_LANG_max_image_width', "Maximum image width (px)");
+define('OGP_LANG_gallery_theme', "Image gallery skin");
+define('OGP_LANG_images_bottom', "Position of the images gallery");
+define('OGP_LANG_img_bottom', "Under the article");
+define('OGP_LANG_img_right', "Right side of the article");
+define('OGP_LANG_no_word', "No");
+define('OGP_LANG_yes_word', "Yes");
+define('OGP_LANG_no_access', "You do not have the right to access this page. Your action will be logged for further inspection.");
+define('OGP_LANG_write_permission_required', "Write permission required");
+define('OGP_LANG_fix_permission', "Please fix permissions. The module will not work as intended until you fix all permissions.");
+define('OGP_LANG_check_permissions', "Check Permissions");
+define('OGP_LANG_permission_ok', "Required permissions are all OK!");
+define('OGP_LANG_empty_title', "Please fill the title");
+define('OGP_LANG_empty_description', "Please fill the content");
+define('OGP_LANG_empty_author', "Please fill the author name");
+define('OGP_LANG_gd_fail', "GD extension is NOT loaded on your server. Images upload disabled.");
+define('OGP_LANG_search_news', "Search the News");
+define('OGP_LANG_help', "help");
+define('OGP_LANG_help_date', "Get help regarding different formating of the date");
+define('OGP_LANG_id_invalid', "The News ID does not exist");
+define('OGP_LANG_id_not_set', "The News ID isn't set");
+define('OGP_LANG_unauthorized_access', "Unauthorized access from");
+define('OGP_LANG_wysiwyg', "WYSIWYG editor");
+define('OGP_LANG_tinymce_lang', "Tiny MCE language");
+define('OGP_LANG_da', "Danish");
+define('OGP_LANG_de', "German");
+define('OGP_LANG_en_GB', "English");
+define('OGP_LANG_es', "Spanish");
+define('OGP_LANG_fi', "Finnish");
+define('OGP_LANG_fr_FR', "French");
+define('OGP_LANG_it', "Italian");
+define('OGP_LANG_pl', "Polish");
+define('OGP_LANG_pt_PT', "Portuguese");
+define('OGP_LANG_ru', "Russian");
+define('OGP_LANG_tinymce_skin', "Tiny MCE skin");
+define('OGP_LANG_tinymce_skin_custom', "You absolutely need to upload your own custom skin in <b>modules/news/js/tinymce/skins/custom/</b> folder to be able to use this skin. If you select it without doing so, you'll encounter problems. Create your own custom skin here <a href='http://skin.tinymce.com/' target='_blank'>http://skin.tinymce.com/</a>.");
+define('OGP_LANG_safe_HTML', "HTML Purifier");
+define('OGP_LANG_safe_HTML_en', "HTML Purifier enabled");
+define('OGP_LANG_safe_HTML_dis', "HTML Purifier disabled");
+define('OGP_LANG_safe_HTML_en_info', "The HTML content of the article in the detailed view will be purified. This will lead in the removal of some HTML tags like iframes. Edit the file <b>modules/news/config.php</b> to change the setting 'safe_HTML' from value '1' (enabled) to value '0' (disabled) to disable this bahavior and allow usage of full HTML without restriction.");
+define('OGP_LANG_safe_HTML_dis_info', "The HTML content of the article in the detailed view will not be purified. Edit the file <b>modules/news/config.php</b> to change the setting 'safe_HTML' from value '0' (disabled) to value '1' (enabled) to enable safe HTML tags usage only.");
+?>
