a1f4383d26ef8378fdfa9ded3279da2e750feda3
own3mall <own3mall@gmail.com>
2024-02-26 21:21:56 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Chinese(China)/modules/news.php
@@ -0,0 +1,98 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2017 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_manage_listings', "管理您的新闻");
+define('OGP_LANG_add_new_listing', "发布新闻");
+define('OGP_LANG_your_current_listings', "您当前的新闻");
+define('OGP_LANG_edit', "编辑");
+define('OGP_LANG_date', "日期");
+define('OGP_LANG_images', "图片");
+define('OGP_LANG_title', "标题");
+define('OGP_LANG_description', "新闻内容");
+define('OGP_LANG_written_by', "作者");
+define('OGP_LANG_details', "阅读更多");
+define('OGP_LANG_modify', "修改");
+define('OGP_LANG_save', "保存");
+define('OGP_LANG_delete', "删除");
+define('OGP_LANG_sure_to_delete', "您确定要删除这些新闻吗？");
+define('OGP_LANG_go_back', "返回");
+define('OGP_LANG_new_added_success', "新闻已成功发布！");
+define('OGP_LANG_add_another', "再添加一个");
+define('OGP_LANG_or_message', "或");
+define('OGP_LANG_please_select', "请选择");
+define('OGP_LANG_submit', "提交");
+define('OGP_LANG_edit_listing', "编辑新闻");
+define('OGP_LANG_modifications_saved', "新的值已成功保存！");
+define('OGP_LANG_modify_images', "修改新文章的图片");
+define('OGP_LANG_upload_more_images', "上传更多图片");
+define('OGP_LANG_latest_news', "最新新闻");
+define('OGP_LANG_search_results', "搜索结果");
+define('OGP_LANG_no_results', "没有找到新闻。");
+define('OGP_LANG_config_options', "新闻选项");
+define('OGP_LANG_date_format', "日期格式");
+define('OGP_LANG_results_per_page', "每页新闻数");
+define('OGP_LANG_enable_search', "启用搜索引擎");
+define('OGP_LANG_image_quality', "图片质量（0-100）");
+define('OGP_LANG_max_image_width', "最大图片宽度（px）");
+define('OGP_LANG_gallery_theme', "图片库皮肤");
+define('OGP_LANG_images_bottom', "图片库的位置");
+define('OGP_LANG_img_bottom', "文章下方");
+define('OGP_LANG_img_right', "文章右侧");
+define('OGP_LANG_no_word', "否");
+define('OGP_LANG_yes_word', "是");
+define('OGP_LANG_no_access', "您没有权限访问此页面。您的操作将被记录以供进一步检查。");
+define('OGP_LANG_write_permission_required', "需要写入权限");
+define('OGP_LANG_fix_permission', "请修复权限。在您修复所有权限之前，模块将无法正常工作。");
+define('OGP_LANG_check_permissions', "检查权限");
+define('OGP_LANG_permission_ok', "所需的权限都正常！");
+define('OGP_LANG_empty_title', "请填写标题");
+define('OGP_LANG_empty_description', "请填写内容");
+define('OGP_LANG_empty_author', "请填写作者名称");
+define('OGP_LANG_gd_fail', "GD扩展在您的服务器上未加载。禁用图片上传。");
+define('OGP_LANG_search_news', "搜索新闻");
+define('OGP_LANG_help', "帮助");
+define('OGP_LANG_help_date', "获取有关不同日期格式的帮助");
+define('OGP_LANG_id_invalid', "新闻ID不存在");
+define('OGP_LANG_id_not_set', "新闻ID未设置");
+define('OGP_LANG_unauthorized_access', "未授权访问来自");
+define('OGP_LANG_wysiwyg', "所见即所得编辑器");
+define('OGP_LANG_tinymce_lang', "Tiny MCE语言");
+define('OGP_LANG_da', "丹麦语");
+define('OGP_LANG_de', "德语");
+define('OGP_LANG_en_GB', "英语");
+define('OGP_LANG_es', "西班牙语");
+define('OGP_LANG_fi', "芬兰语");
+define('OGP_LANG_fr_FR', "法语");
+define('OGP_LANG_it', "意大利语");
+define('OGP_LANG_pl', "波兰语");
+define('OGP_LANG_pt_PT', "葡萄牙语");
+define('OGP_LANG_ru', "俄语");
+define('OGP_LANG_tinymce_skin', "Tiny MCE皮肤");
+define('OGP_LANG_tinymce_skin_custom', "您绝对需要在<b>modules/news/js/tinymce/skins/custom/</b>文件夹中上传您自己的自定义皮肤才能使用此皮肤。如果您在未这样做的情况下选择它，您将遇到问题。在这里创建您自己的自定义皮肤<a href='http://skin.tinymce.com/' target='_blank'>http://skin.tinymce.com/</a>。");
+define('OGP_LANG_safe_HTML', "HTML Purifier");
+define('OGP_LANG_safe_HTML_en', "HTML Purifier 启用");
+define('OGP_LANG_safe_HTML_dis', "HTML Purifier 已禁用");
+define('OGP_LANG_safe_HTML_en_info', "文章的详细视图中的HTML内容将被净化。这将导致一些HTML标签如iframe被移除。在<b>modules/news/config.php</b>文件中更改'safe_HTML'的设置，从值'1'（启用）更改为值'0'（禁用），以禁用此行为并允许不受限制地使用完整的HTML。");
+define('OGP_LANG_safe_HTML_dis_info', "文章的详细视图中的HTML内容将不会被净化。在<b>modules/news/config.php</b>文件中更改'safe_HTML'的设置，从值'0'（禁用）更改为值'1'（启用），以启用仅允许安全HTML标签的使用。");
+?>
